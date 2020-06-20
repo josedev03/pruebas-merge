82a41fc77fb2e4e4c99271d17e68180bf58d7359
Rodriguez <joseluis.rodriguez@dxc.com>
2020-06-20 13:42:02 -0500

    agregando saludos

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 class Saludo{
+  public String tercerSaludo(string inicial, string final){
+    console.log("mostrando el mensaje 1");
+    console.log("1087878");
+    let padre  "lslakalklsk";
+  }
+
   public String saludoInicial(string inicial, string final){
+    console.log("mostrando el mensaje 1");
+    console.log("1087878");
+    let padre  "lslakalklsk";
+  }
+
+  public String cuartoSaludo(string inicial, string final){
     console.log("mostrando el mensaje 1");
     console.log("1087878");
     let padre  "lslakalklsk";
