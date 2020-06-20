42c9084c7b55e0c95f702cde883b8eced3f4e819
Rodriguez <joseluis.rodriguez@dxc.com>
2020-06-20 13:54:03 -0500

    agregando archivo

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ class Saludo{
     let padre  "lslakalklsk";
   }
 
+  public String segundoSaludo(string inicial, number final, string desconocido){
+    let padre  "lslakalklsk";
+    let suma = 2+2;
+    console.log("mostrando el mensaje 1");
+    console.log("1087878");
+    let suma3 = 2+2;
+    console.log(suma3);
+  }
+
   public String cuartoSaludo(string inicial, number final){
     console.log("mostrando el mensaje 1");
     console.log("1087878");
