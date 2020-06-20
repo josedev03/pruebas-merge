f853639027eb4df00a4ae962872b6e7e3a9622d1
Rodriguez <joseluis.rodriguez@dxc.com>
2020-06-20 13:40:37 -0500

    commit inicial

---
+++ index.js
@@ -0,0 +1,13 @@
+class Saludo{
+  public String saludoInicial(string inicial, string final){
+    console.log("mostrando el mensaje 1");
+    console.log("1087878");
+    let padre  "lslakalklsk";
+  }
+
+  public String segundoSaludo(string inicial, string final){
+    console.log("mostrando el mensaje 1");
+    console.log("1087878");
+    let padre  "lslakalklsk";
+  }
+}
